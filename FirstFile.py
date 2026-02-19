5de71ba5a4163859551b5cee42338c1564b796f4
LocationNotFound <ethanliu186@gmail.com>
2026-02-19 16:13:43 -0500

    Update FirstFile.py

--- FirstFile.py
+++ FirstFile.py
@@ -1,5 +1,13 @@
 from typing import final
 
+@final
+class RandomClass:
+  pass
+
+#Apparently, this should result in error
+#class RandomClasses(RandomClass):
+  #pass
+
 
 def sum(*args):
   result = 0
